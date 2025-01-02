ae2e7abfbc4e776b09df4ee78381ea39d6e8c158
aybukeberfinsuer <aybukeberfinsuer@gmail.com>
2025-01-02 22:06:56 +0300

    interfaces

---
+++ Cental.DataAccessLayer/Repositories/GenericRepository.cs
@@ -0,0 +1,50 @@
+﻿using Cental.DataAccessLayer.Abstract;
+using Cental.DataAccessLayer.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cental.DataAccessLayer.Repositories
+{
+	public class GenericRepository<T> : IGenericDal<T> where T : class
+	{
+		private readonly CentalContext _centalContext;
+
+		public GenericRepository(CentalContext centalContext)
+		{
+			_centalContext = centalContext;
+		}
+
+		public void Create(T entity)
+		{
+			_centalContext.Add(entity);
+			_centalContext.SaveChanges();
+		}
+
+		public void Delete(int id)
+		{
+			var value= GetById(id);
+			_centalContext.Remove(value);
+			_centalContext.SaveChanges();
+			
+		}
+
+		public List<T> GetAll()
+		{
+			return _centalContext.Set<T>().ToList();
+		}
+
+		public T GetById(int id)
+		{
+			return _centalContext.Set<T>().Find(id);
+		}
+
+		public void Update(T entity)
+		{
+			_centalContext.Update(entity);
+			_centalContext.SaveChanges();
+		}
+	}
+}
